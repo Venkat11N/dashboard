76666054c21d115bfc23c1fec6ac0f61eccf4b44
Venkat11N <vnaiduari@gmail.com>
2026-03-12 16:52:22 +0530

    update package.json and index.ts

--- grievance-backend/src/config/index.ts
+++ grievance-backend/src/config/index.ts
@@ -6,11 +6,11 @@ export default {
     port: process.env.PORT || 5000,
   },
   db: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'maritime_portal',
-    port: Number(process.env.DB_PORT) || 3307,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 3306,
   },
   jwtSecret: process.env.JWT_SECRET || 'dev_secret_key'
 };
