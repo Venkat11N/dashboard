ebadc230d3e09fd81b1497688514fca44c4ade4f
Venkat11N <vnaiduari@gmail.com>
2026-03-12 17:11:08 +0530

    updated docker files

--- grievance-backend/src/config/index.ts
+++ grievance-backend/src/config/index.ts
@@ -6,7 +6,7 @@ export default {
     port: process.env.PORT || 5000,
   },
   db: {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
